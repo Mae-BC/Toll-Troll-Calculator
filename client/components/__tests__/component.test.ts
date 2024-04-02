38ec8140ea815f8c06b9f17ae04d1a9559766ce6
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-03 12:42:06 +1300

    starting testing

--- client/components/__tests__/component.test.ts
+++ client/components/__tests__/component.test.ts
@@ -1,34 +1,8 @@
-import { describe, it, expect } from "vitest"
-
-import App from '../components/App'
-import { render } from "react-dom"
-
-
-describe('<App />', () => {
-  it('renders a heading', () => {
-    // ARRANGE
-    const { container } = render(<App />)
-
-    // ACT
-    // we don't have anything to do
-    // ASSERT
-    const heading = screen.queryByRole('heading')
-    expect(heading).toHaveTextContent('Cats')
-  })
-
-  it.todo('renders how we expect', () => {
-    // ARRANGE
-    const { container } = render(<App />)
-
-    // ACT
-    // we don't have anything to do
-    // ASSERT
-    expect(container).toMatchSnapshot()
-  })
-
-  it.todo('renders an image of a bulldog', () => {
-    render(<App />)
-    const image = screen.getByAltText('a Bulldog')
-    expect(image).toBeVisible()
-  })
-})
+import { beforeEach, it, expect } from 'vitest'
+import { renderRoute } from '../../test/setup.tsx'
+import App from '../App'
+import * as api from '../api/bridge'
+import { render } from 'react-dom'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import SingleBridge from './SingleBridge'
