729b2dd0cec11ff821769d33b079b595f891ced0
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-03 10:25:22 +1300

    made a link from single bridge to all bridges

--- client/components/Bridges.tsx
+++ client/components/Bridges.tsx
@@ -1,3 +1,4 @@
+import { Link, useParams } from 'react-router-dom'
 import { Bridge } from '../../models/bridge.ts'
 import { getBridges } from '../api/bridge.ts'
 import { useQuery } from '@tanstack/react-query'
@@ -9,19 +10,26 @@ export default function Bridges() {
     isLoading,
   } = useQuery({ queryKey: ['bridges'], queryFn: getBridges })
 
+  const { id } = useParams()
+
   if (error) {
     return <p>Your bridges are gone! What a massive error</p>
   }
   if (!bridges || isLoading) {
     return <p>Fetching bridges from auckland...</p>
   }
+  console.log(bridges)
 
   return (
     <>
       <h1>Bridge Toll Calculater 🧌</h1>
       <ul>
-        {bridges.map((br) => {
-          return <li key={br.id}>{br.name}</li>
+        {bridges.map((bridge) => {
+          return (
+            <li key={bridge.id}>
+              <Link to={`/${bridge.id}`}>{bridge.name}</Link>
+            </li>
+          )
         })}
       </ul>
     </>
