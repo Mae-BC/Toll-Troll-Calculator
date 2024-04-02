11682aa8ae4f5e030d38089cf6c6680ca3601c09
tempo <tempo@PJ-766262-K01>
2024-04-03 11:47:38 +1300

    wip

---
+++ client/components/__tests__/login.test.tsx
@@ -0,0 +1,98 @@
+//@vitest-environment jsdom
+
+import {
+  describe,
+  it,
+  expect,
+  vi,
+  beforeEach,
+  afterEach,
+  beforeAll,
+} from 'vitest'
+import {
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+} from '@testing-library/react'
+import { renderRoute } from '../../test/setup.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
+import nock from 'nock'
+
+const mockBridges = {
+  bridges: [
+    {
+      name: 'Auckland Harbour Bridge',
+      location: 'Auckland Harbour',
+      type: 'Motorway bridge',
+      year_built: 1959,
+      length_meters: 1020,
+      lanes: 8,
+      added_by_user: null,
+    },
+    {
+      name: 'Grafton Bridge',
+      location: 'Grafton Gully',
+      type: 'Road bridge',
+      year_built: 1910,
+      length_meters: 100,
+      lanes: 4,
+      added_by_user: null,
+    },
+  ],
+}
+
+// Mock out auth0
+vi.mock('@auth0/auth0-react')
+
+const ACCESS_TOKEN = 'mock-access-token'
+
+beforeAll(() => {
+  nock.disableNetConnect()
+
+  // Add to remove errors in test output
+  vi.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+beforeEach(() => {
+  vi.mocked(useAuth0).mockReturnValue({
+    isAuthenticated: true,
+    user: { sub: 'bear@example.com', nickname: 'bear' },
+    getAccessTokenSilently: vi.fn().mockReturnValue(ACCESS_TOKEN),
+    loginWithRedirect: vi.fn(),
+    logout: vi.fn(),
+  } as any)
+})
+
+afterEach(() => {
+  vi.clearAllMocks()
+})
+
+describe('<Bridges />', () => {
+
+  it('should render the title', async () => {
+    renderRoute('/')
+    const title = screen.getByRole('heading', {level: 1})
+    expect(title).toHaveTextContent('Bridge Toll Calculator')
+  })
+
+  it('should render some bridges', async () => {
+    const scope = nock('http://localhost')
+      .get('/api/v1/bridges')
+      .reply(200, mockBridges)
+
+    renderRoute('/')
+
+    const list = (await screen.findAllByRole('list')) as HTMLElement[]
+    const listItems = within(list[0])
+      .getAllByRole('listitem')
+      .map((li) => li.textContent)
+    expect(listItems).toMatchInlineSnapshot(`
+    [
+      "Auckland Harbour Bridge",
+      "Grafton Bridge",
+    ]
+  `)
+    expect(scope.isDone()).toBe(true)
+  })
+})
