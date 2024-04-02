f18dcf70e06600d626da7d2d3c9ba85899b62a8c
Jack Crawford <140600689+jack-crawford-1@users.noreply.github.com>
2024-04-02 17:45:40 +1300

    showing all bridge data in SingleBridge component

--- client/components/SingleBridge.tsx
+++ client/components/SingleBridge.tsx
@@ -17,11 +17,20 @@ function SingleBridge() {
   if (isLoading) return <div>Loading...</div>
   if (error || !bridge) return <div>Error</div>
 
+  // need to use snake case format from db table for yearBuilt, LengthMeters and AddByUser.
+  // Possibly need to update Bridge Model to remove errors below:
   return (
     <div>
       <h1>This is a single Bridge</h1>
       <ul>
-        <li>{bridge.name}</li>
+        <li>Bridge Name: {bridge.name}</li>
+        <li>ID: {bridge.id}</li>
+        <li>Location: {bridge.location}</li>
+        <li>Type: {bridge.type}</li>
+        <li>Year Built: {bridge.year_built}</li>
+        <li>Length(meters): {bridge.length_meters}</li>
+        <li>Lanes: {bridge.lanes}</li>
+        <li>Added By: {bridge.added_by_user}</li>
       </ul>
     </div>
   )
