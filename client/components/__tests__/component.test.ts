5380e0434ec86249331347f27e8493b24d2e820e
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-03 15:06:44 +1300

    wrote first test for loading state

--- client/components/__tests__/component.test.ts
+++ client/components/__tests__/component.test.ts
@@ -1,8 +1,42 @@
+//@vitest-environment jsdom
+
 import { beforeEach, it, expect } from 'vitest'
 import { renderRoute } from '../../test/setup.tsx'
-import App from '../App'
-import * as api from '../api/bridge'
 import { render } from 'react-dom'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import SingleBridge from './SingleBridge'
+import nock from 'nock'
+import {
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+} from '@testing-library/react/pure'
+
+nock.disableNetConnect()
+
+const mockBridgeData = {
+  id: 1,
+  name: 'Mock Bridge',
+  location: 'Mock Location',
+  type: 'Mock Type',
+  yearBuilt: 3000,
+  lengthMeters: 100,
+  lanes: 2,
+  addedByUser: 'Mock User',
+}
+
+it('renders loading state initially', async () => {
+  const scope = nock('http://localhost')
+    .get(`/api/v1/bridges/1`)
+    .reply(200, mockBridgeData)
+
+  // Act
+  const { ...screen } = renderRoute('/1')
+
+  // Assert
+  // const loading = await waitFor(() => screen.getByLabelText(/loading/i))
+  const loading = await screen.findByText(/loading/i)
+
+  expect(loading).toBeVisible()
+})
