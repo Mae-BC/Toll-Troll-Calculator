09d2d2466947d0ffd976907fceca16c8516aa4d9
Keanu Carnevale <keanucarnevale@gmail.com>
2024-04-03 17:34:45 +1300

    cleaned up files for submission

--- client/components/__tests__/SingleBridge.test.ts
+++ client/components/__tests__/SingleBridge.test.ts
@@ -1,17 +1,8 @@
 //@vitest-environment jsdom
 
-import { beforeEach, it, expect } from 'vitest'
+import { it, expect } from 'vitest'
 import { renderRoute } from '../../test/setup.tsx'
-import { render } from 'react-dom'
-import { useParams } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
-import SingleBridge from './SingleBridge'
 import nock from 'nock'
-import {
-  waitFor,
-  waitForElementToBeRemoved,
-  within,
-} from '@testing-library/react/pure'
 
 nock.disableNetConnect()
 
@@ -31,12 +22,21 @@ it('renders loading state initially', async () => {
     .get(`/api/v1/bridges/1`)
     .reply(200, mockBridgeData)
 
-  // Act
   const { ...screen } = renderRoute('/1')
 
-  // Assert
-  // const loading = await waitFor(() => screen.getByLabelText(/loading/i))
   const loading = await screen.findByText(/loading/i)
 
   expect(loading).toBeVisible()
+})
+
+it('should show the bridge has an ID', async () => {
+  const scope = nock('http://localhost')
+    .get(`/api/v1/bridges/1`)
+    .reply(200, mockBridgeData)
+
+  const { ...screen } = renderRoute('/14')
+
+  const name = await screen.findByText(/ID/i)
+
+  expect(name).toBeVisible()
 })
